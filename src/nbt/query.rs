716ad8bb7db34e1e74144f4ac6e985230f9d328d
jntun <justin@jntun.com>
2024-08-08 23:57:00 -0700

    nbt: implement find_many_by_name() & Display for Error

--- src/nbt/query.rs
+++ src/nbt/query.rs
@@ -2,15 +2,39 @@
 // Created by Justin Tunheim on 7/22/24
 //
 
-use crate::nbt::{ NBT,TAGString };
+use crate::nbt::{ NBT, Payload, TAGString };
 
 pub enum Error {
+    NotFound
 }
 
 pub(crate) fn find_first_by_name(name: TAGString, root: &NBT) -> Result<NBT, Error> {
     Ok(NBT::default())
 }
 
-pub(crate) fn find_many_by_name(name: TAGString, root: &NBT) -> Result<Vec<NBT>, Error> {
-    Ok(Vec::new())
+pub(crate) fn find_many_by_name(name: &TAGString, root: NBT) -> Result<Vec<NBT>, Error> {
+    let mut tags = Vec::new();
+
+    if root.name == *name {
+        tags.push(root);
+    } else {
+        match root.payload {
+            Payload::Compound(compound_tag) => {
+                for nbt in compound_tag.tags {
+                    tags.append(&mut find_many_by_name(name, nbt)?);
+                }
+            },
+            _ => return Err(Error::NotFound),
+        };
+    }
+
+    Ok(tags)
+}
+
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::NotFound => f.write_str("not found")
+        }
+    }
 }
