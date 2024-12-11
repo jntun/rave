36f510b6bedda1ea8ec469b0629f81708fd2ff4d
Justin Tunheim <justin@unsupportedtransit.com>
2024-12-10 18:01:03 -0800

    gestalt: begin "list" cmd implementation & temporarily remove search index

--- src/gestalt.rs
+++ src/gestalt.rs
@@ -9,15 +9,15 @@ mod directory {
 
     pub(crate) type List = Vec<String>;
 
-    pub(crate) struct WorldFileBundle {
+    pub(crate) struct WorldFile {
         pub overworld: List,
         pub nether:    List,
         pub the_end:   List,
     }
 
-    pub(crate) fn region_files(config: &config::Configuration) -> WorldFileBundle {
+	pub(crate) fn region_files(config: &config::Configuration) -> WorldFile {
         let base = config.save_root.value().unwrap();
-        let mut bundle = WorldFileBundle{
+        let mut bundle = WorldFile{
             overworld: Vec::new(), 
             nether: Vec::new(),
             the_end: Vec::new(),
@@ -27,7 +27,9 @@ mod directory {
         bundle.the_end.push(format!("{}/DIM1/region", base));
         bundle
     }
+
 }
+
 
 pub enum Error {
     Search(nbt::query::Error),
@@ -40,15 +42,57 @@ pub enum Error {
 struct Gestalt {
 }
 
+fn plural(i: usize) -> char {
+	if i == 1 {
+		' '
+	} else {
+		's'
+	}
+}
+
 pub fn run(config: config::Configuration) -> Result<(), Error> {
     let mut gestalt = Gestalt::new(&config);
 
+
     match config.command.value().unwrap() {
-        config::Command::List(_) => todo!(),
+        config::Command::List(_)   => gestalt.list(config)?,
         config::Command::Search(_) => gestalt.search(config)?,
     };
 
     Ok(())
+}
+
+pub(crate) fn chunks_in_world(world: directory::List) -> Result<Vec<region::Chunk>, Error> {
+	let mut chunks = Vec::new();
+	for dir in world {
+		let files = match std::fs::read_dir(dir) {
+			Ok(f) => f,
+			Err(e) => return Err(Error::ReadFile(e)),
+		};
+		for file in files {
+			let buffer = match std::fs::read(file.unwrap().path()) {
+				Ok(b) => b,
+				Err(e) => return Err(Error::ReadFile(e)),
+			};
+			let mut region = match region::Parser::new(buffer).parse() {
+				Ok(r) => r,
+				Err(e) => return Err(Error::Region(e)),
+			};
+			chunks.append(&mut region);
+		}
+	}
+	Ok(chunks)
+}
+
+pub(crate) fn chunks_in_bundle(world_bundle: &directory::WorldFile) -> Result<Vec<region::Chunk>, Error> {
+	let mut chunks = Vec::new();
+	println!("starting overworld...");
+	chunks.append(&mut chunks_in_world(world_bundle.overworld.clone())?);
+	println!("overworld done. starting nether...");
+	chunks.append(&mut chunks_in_world(world_bundle.nether.clone())?);
+	println!("nether done. starting the end...");
+	chunks.append(&mut chunks_in_world(world_bundle.the_end.clone())?);
+	Ok(chunks)
 }
 
 impl Gestalt {
@@ -57,20 +101,25 @@ impl Gestalt {
     }
 
     fn find_nbt(&mut self, config: &config::Configuration, file_list: directory::List) -> Result<(), Error> {
-        println!("finding: {:?}", file_list);
         let method = match config.command.value().unwrap() {
             config::Command::Search(method) => method,
             _ => return Err(Error::Command(format!("Gestalt::find() should not be called on anything but a config::Command::Search"))), 
         };
-        let Some(idx) = config.index.value() else {
-            return self.fast_find(method);
-        };
-        match idx {
-            config::Index::First => return self.fast_find(method),
-            _ => (),
-        }
 
-        let mut results= Vec::new();
+/*
+*       let Some(idx) = config.index.value() else {
+*           return self.fast_find(method);
+*       };
+*
+*       match idx {
+*           config::Index::First => return self.fast_find(method),
+*           _ => (),
+*       }
+*/
+
+        let mut results: Vec<nbt::NBT> = Vec::new();
+		let mut visited_nodes = 0;
+		
         for dir in file_list {
             let files = match std::fs::read_dir(dir) {
                 Ok(f) => f,
@@ -81,46 +130,66 @@ impl Gestalt {
                     Ok(b) => b,
                     Err(e) => return Err(Error::ReadFile(e)),
                 };
-                let region= match region::Parser::new(buffer).parse() {
+                let region = match region::Parser::new(buffer).parse() {
                     Ok(r) => r,
                     Err(e) => return Err(Error::Region(e)),
                 };
                 for chunk in region {
-                    let nbts = match method {
+                    match method {
                         config::Method::Name(name) => {
                             match nbt::query::find_many_by_name(&nbt::TAGString::from(name.clone()), chunk.nbt_owned()) {
-                                Ok(nbts) => nbts,
-                                Err(e) => return Err(Error::Search(e)),
+                                Ok(mut nbts) => {
+									results.append(&mut nbts);
+								}
+                                Err(e) => {
+									match e {
+										nbt::query::Error::NotFound => {
+											visited_nodes += 1;
+											continue;
+										},
+									}
+								},
                             }
                         }
-                    };
-
-                    results.push(nbts);
-
-                    match idx {
-                        config::Index::Value(i) => {
-                            if &results.len() == i {
-                                break;
-                            }
-                        },
-                        _ => (),
                     }
+
+/*
+*                   match idx {
+*                       config::Index::Value(i) => {
+*                           if &results.len() == i {
+*                               break;
+*                           }
+*                       },
+*                       _ => (),
+*                   }
+*/
+
                 }
             }
         }
-
+		println!("searched: {} node{}", visited_nodes, plural(visited_nodes));
         Ok(())
     }
 
     fn search(&mut self, config: config::Configuration) -> Result<(), Error> {
         let region_files = directory::region_files(&config);
-        println!("{}\n{:?}", "starting...", config);
+        println!("{}\n{:?}", "starting search...", config);
         self.find_nbt(&config, region_files.overworld)?;
         self.find_nbt(&config, region_files.nether)?;
         self.find_nbt(&config, region_files.the_end)?;
-        println!("{}", "ending...");
+        println!("{}", "finished search.");
         Ok(())
     }
+
+	fn list(&mut self, config: config::Configuration) -> Result<(), Error> {
+		let save_dir = directory::region_files(&config);
+		let chunks = chunks_in_bundle(&save_dir)?;
+		for chunk in chunks {
+			println!("{}", chunk.nbt());
+		}
+		Ok(())
+	}
+
 }
 
 impl Gestalt {
